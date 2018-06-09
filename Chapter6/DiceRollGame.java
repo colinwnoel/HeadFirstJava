54c30833c6d1f4765e6e9089e51531f56c92ce13
Colin Noel <cwnoel@sbgtv.com>
2018-06-08 18:55:42 -0700

    Added some comments and moved most of the logic outside the main method back into determineWinner()

--- Chapter6/DiceRollGame.java
+++ Chapter6/DiceRollGame.java
@@ -17,19 +17,25 @@ public class DiceRollGame {
     }
 
     public void determineWinner(Dice aDice){
+
+        //Creating an ArrayList of ValueOccurrances - 6 of them, one for each possible value of a die roll
         ArrayList<ValueOccurrance> listOccurrance = new ArrayList<>();
         for (int index = 1; index < 7; index++) {
             listOccurrance.add(new ValueOccurrance(index, 0));
         }
+        //5 Dice in a roll we'll start at 5 and go down from there
         int numOfDice = 5;
         while (numOfDice > 0) {
+            //Get a random value and assign
             int randomValue = aDice.enhancedRollDice();
+            //Get the value we want to increment and use setValue which increases the count by 1
             ValueOccurrance myOccur = listOccurrance.get(randomValue - 1);
             myOccur.setValue();
             System.out.println("We rolled a " + randomValue);
             numOfDice--;
         }
         System.out.println();
+        // need some variables to keep track of how many of each winning roll we have
         int numPairs = 0;
         int numThrees = 0;
         int numFours = 0;
@@ -52,24 +58,22 @@ public class DiceRollGame {
                 }
             }
         }
+        //handling pairs
         if (numPairs > 1) {
             System.out.println("two pair");
-        }
-
-        if (numPairs == 1 && numThrees == 1) {
+            //handling a full house - 1 pair 1 three of a kind
+        }else if (numPairs == 1 && numThrees == 1) {
             System.out.println("full house");
         }
-
+        //handling 4 of a kind
         if (numFours == 1) {
             System.out.println("four of a kind");
         }
-
+        //handling 3 of a kind
         if (numThrees == 1) {
             System.out.println("three of a kind");
         }
 
-
-        System.out.println();
     }
 
 
